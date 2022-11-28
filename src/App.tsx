c2fa9f41fa8d028e7d04cf5c8d48377561fe4c55
victorscarmo <victorsilvacarmo@gmail.com>
2022-11-27 21:12:02 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import * as C from "./App.styles";
+import { Character } from "./components/Character/Character";
+import { useCharacter } from "./components/Character/Character.logic";
 
-function App() {
+const App = () => {
+  const char = useCharacter();
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.code) {
+      case "KeyA":
+      case "ArrowLeft":
+        char.moveLeft();
+        break;
+      case "KeyD":
+      case "ArrowRight":
+        char.moveRight();
+        break;
+      case "KeyW":
+      case "ArrowUp":
+        char.moveUp();
+        break;
+      case "KeyS":
+      case "ArrowDown":
+        char.moveDown();
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side} />
+      </C.Map>
+    </C.Container>
   );
-}
+};
 
 export default App;
